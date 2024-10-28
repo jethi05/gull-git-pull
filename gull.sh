66975e93b886a19fc7f0b83e2e0e763b53476272
jethi05 <jethi.gei@gmail.com>
2024-10-28 22:37:11 +0100

    .

--- gull.sh
+++ gull.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env bash
-# JETHI 05
+#!/bin/bash
 
+# List all directories in the specified path
+PULL_DIC="$(ls -d /home/jeremia/dev/*/)"
+echo "Repositories found:"
 
-PULL_DIC="$(ls -l /home/jeremia/development/)"
-echo "$PULL_DIC"
-for REPO in "$PULL_DIC"; do
-    echo "$(ls -l $REPO)"
+for REPO in $PULL_DIC; do
+    echo "Pulling updates for: $REPO"
+    # Navigate to the repository directory and pull changes
+    cd "$REPO" && git pull
+    if [ $? -eq 0 ]; then
+        echo "Successfully pulled updates for: $REPO"
+    else
+        echo "Failed to pull updates for: $REPO"
+    fi
 done
+
